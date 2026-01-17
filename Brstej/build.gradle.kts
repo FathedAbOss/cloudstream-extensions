255583f0c7db5f5248dd761d737e945493177b7d
FathedAbOss <fathekhateb22@gmail.com>
2026-01-17 23:13:22 +0000

    Create build.gradle.kts

---
+++ Brstej/build.gradle.kts
@@ -0,0 +1,41 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+android {
+    namespace = "com.example"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+            freeCompilerArgs = freeCompilerArgs + "-Xjvm-default=all"
+        }
+    }
+}
+
+cloudstream {
+    // Set the package name of your provider
+    setPackage("com.example")
+    // Set the class name of your provider
+    setProviderClass("BrstejProvider")
+}
+
+dependencies {
+    val cloudstream_version = "3.0.0"
+    compileOnly("com.lagradost:cloudstream3:$cloudstream_version")
+}
