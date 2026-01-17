7a0238d2a5993e0dbc8b3a4599b75e5f74238e30
FathedAbOss <fathekhateb22@gmail.com>
2026-01-17 22:39:28 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,12 +38,11 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "FathedAbOss/cloudstream-extensions")
     }
 
     android {
-        namespace = "com.example"
-
+        namespace = "com.fathedaboss"
         defaultConfig {
             minSdk = 21
             compileSdkVersion(35)
@@ -66,26 +65,4 @@ subprojects {
             }
         }
     }
-
-    dependencies {
-        val cloudstream by configurations
-        val implementation by configurations
-
-        // Stubs for all cloudstream classes
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        // These dependencies can include any of those which are added by the app,
-        // but you don't need to include any of them if you don't need them.
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle.kts
-        implementation(kotlin("stdlib")) // Adds Standard Kotlin Features
-        implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP Lib
-        implementation("org.jsoup:jsoup:1.18.3") // HTML Parser
-        // IMPORTANT: Do not bump Jackson above 2.13.1, as newer versions will
-        // break compatibility on older Android devices.
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") // JSON Parser
-    }
-}
-
-task<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
