805780b8239f5412581f94e641c1c09a9e314d85
FathedAbOss <fathekhateb22@gmail.com>
2026-01-18 11:51:54 +0100

    Update build.gradle.kts

--- JustWatch/build.gradle.kts
+++ JustWatch/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -37,15 +36,9 @@ cloudstream {
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://raw.githubusercontent.com/recloudstream/cloudstream/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png"
-
-    setPackage("com.example")
-    setProviderClass("JustWatchProvider")
 }
 
 dependencies {
     val cloudstream_version = "4.1.1"
     compileOnly("com.lagradost:cloudstream3:$cloudstream_version")
 }
-
-setPackage("com.example")
-setProviderClass("JustWatchProvider")
