c6f054128924af9815fc071756ffb0a942f5329d
FathedAbOss <fathekhateb22@gmail.com>
2026-01-17 16:38:19 -0800

    Create build.gradle.kts

---
+++ AkSv/build.gradle.kts
@@ -0,0 +1,47 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+android {
+    namespace = "com.example"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+            freeCompilerArgs = freeCompilerArgs + "-Xjvm-default=all"
+        }
+    }
+}
+
+cloudstream {
+    language = "ar"
+    version = 1
+    description = "AkSv placeholder provider"
+    authors = listOf("FathedAbOss")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://raw.githubusercontent.com/recloudstream/cloudstream/46f9e36ae0c6b29f012334583b3b57c543f714e1/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png"
+}
+
+dependencies {
+    val cloudstream_version = "3.0.0"
+    compileOnly("com.lagradost:cloudstream3:$cloudstream_version")
+}
+
+setPackage("com.example")
+setProviderClass("AkSvProvider")
