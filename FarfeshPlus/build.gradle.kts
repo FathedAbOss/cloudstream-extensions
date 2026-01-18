150fb44cdd852b73e4a8462f17431360a3c84d86
FathedAbOss <fathekhateb22@gmail.com>
2026-01-17 17:14:23 -0800

    Update FarfeshPlus build.gradle.kts

--- FarfeshPlus/build.gradle.kts
+++ FarfeshPlus/build.gradle.kts
@@ -27,13 +27,21 @@ android {
         }
     }
 }
+version = 1
 
 cloudstream {
-    // Set the package name of your provider
+       // Set the package name of your provider
     setPackage("com.example")
     // Set the class name of your provider
     setProviderClass("FarfeshPlusProvider")
-}
+
+    // Plugin metadata
+    description = "FarfeshPlus placeholder provider"
+    authors = listOf("FathedAbOss")
+    status = 1
+    language = "ar"
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://raw.githubusercontent.com/recloudstream/cloudstream/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png"
 
 dependencies {
     val cloudstream_version = "3.0.0"
