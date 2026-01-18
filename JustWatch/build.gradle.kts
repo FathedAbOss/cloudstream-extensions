48c0047348a44f003d112a712f2acdefa6fbf1b9
FathedAbOss <fathekhateb22@gmail.com>
2026-01-18 04:07:03 +0100

    Update build.gradle.kts

--- JustWatch/build.gradle.kts
+++ JustWatch/build.gradle.kts
@@ -32,11 +32,14 @@ version = 1
 
 cloudstream {
     language = "ar"
-    description = "JustWatch placeholder provider"
+    description = "JustWatch provider"
     authors = listOf("FathedAbOss")
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://raw.githubusercontent.com/recloudstream/cloudstream/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png"
+
+    setPackage("com.example")
+    setProviderClass("JustWatchProvider")
 }
 
 dependencies {
