6f21c0e90a8a1be999b3b96ac969f032369159b6
FathedAbOss <fathekhateb22@gmail.com>
2026-01-23 00:58:02 +0100

    Update CimaLightPlugin.kt

--- CimaLight/src/main/kotlin/com/example/CimaLightPlugin.kt
+++ CimaLight/src/main/kotlin/com/example/CimaLightPlugin.kt
@@ -1,13 +1,12 @@
 package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class CimaLightPlugin: Plugin() {
-      override fun load(context: Context) {
-                // All providers should be registered here
-                registerMainAPI(CimaLightProvider())
-      }
+class CimaLightPlugin: CloudstreamPlugin() {
+    override fun load(context: Context) {
+        // All providers should be registered here
+        registerMainAPI(CimaLightProvider())
+    }
 }
