0178aeb8c83944105a077298b0e2c7cb558e5e0c
FathedAbOss <fathekhateb22@gmail.com>
2026-01-22 19:39:42 +0100

    Update build.gradle.kts

--- Brstej/build.gradle.kts
+++ Brstej/build.gradle.kts
@@ -1,40 +1,33 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-
 plugins {
     id("com.android.library")
     kotlin("android")
     id("com.lagradost.cloudstream3.gradle")
 }
 
-
 android {
-    namespace = "com.example" // Verify this namespace is unique per provider if possible
-    compileSdk = 33
-
+    namespace = "com.fathedaboss.brstej"
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
     }
 
-
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
-
 
     tasks.withType<KotlinCompile> {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
             freeCompilerArgs.add("-Xjvm-default=all")
         }
     }
 }
 
-
 version = "1.0.0"
-
 
 cloudstream {
     language = "ar"
@@ -42,10 +35,11 @@ cloudstream {
     authors = listOf("FathedAbOss")
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
-    iconUrl = "https://raw.githubusercontent.com/recloudstream/cloudstream/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png"
 }
 
-
 dependencies {
-    apk("com.lagradost:cloudstream3:pre-release")
+    val cloudstream_version = "master-SNAPSHOT"
+    compileOnly("com.github.recloudstream:cloudstream3:$cloudstream_version")
+    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("com.github.recloudstream:nicehttp:master-SNAPSHOT" )
 }
