40960936d905923b1918273e0d76c6218ed5d7f3
FathedAbOss <fathekhateb22@gmail.com>
2026-01-22 19:37:42 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,48 +24,38 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = 
-    extensions.getByType<CloudstreamExtension>().configuration()
-
-fun Project.android(configuration: BaseExtension.() -> Unit) = 
-    extensions.getByType<BaseExtension>().configuration()
-
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-    cloudstream {
+    // Configure Android immediately to avoid "compileSdkVersion not specified"
+    configure<BaseExtension> {
+        namespace = "com.fathedaboss.${project.name.lowercase()}"
+        compileSdkVersion(34)
+
+        defaultConfig {
+            minSdk = 21
+            targetSdk = 34
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+
+    // Configure Cloudstream
+    extensions.configure<CloudstreamExtension> {
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "FathedAbOss/cloudstream-extensions")
     }
 
-    // Use afterEvaluate to ensure the android extension is available
-    afterEvaluate {
-        if (project.extensions.findByName("android") != null) {
-            configure<BaseExtension> {
-                namespace = "com.fathedaboss.${project.name.lowercase()}"
-                compileSdkVersion(34)
-
-                defaultConfig {
-                    minSdk = 21
-                    targetSdk = 34
-                }
-
-                compileOptions {
-                    sourceCompatibility = JavaVersion.VERSION_1_8
-                    targetCompatibility = JavaVersion.VERSION_1_8
-                }
-            }
-        }
-        
-        if (project.extensions.findByName("dependencies") != null) {
-            dependencies {
-                val cloudstream_version = "master-SNAPSHOT"
-                add("compileOnly", "com.github.recloudstream:cloudstream3:$cloudstream_version")
-                add("implementation", "org.jsoup:jsoup:1.15.3")
-                add("implementation", "com.github.recloudstream:nicehttp:master-SNAPSHOT")
-            }
-        }
+    // Configure Dependencies
+    dependencies {
+        val cloudstream_version = "master-SNAPSHOT"
+        add("compileOnly", "com.github.recloudstream:cloudstream3:$cloudstream_version")
+        add("implementation", "org.jsoup:jsoup:1.15.3")
+        add("implementation", "com.github.recloudstream:nicehttp:master-SNAPSHOT")
     }
 
     tasks.withType<KotlinCompile> {
