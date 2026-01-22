8e7e68a46cdfb536e73d9142ac9bb575367019bd
FathedAbOss <fathekhateb22@gmail.com>
2026-01-22 20:11:12 +0100

    Update build.gradle.kts

--- ElCinema/build.gradle.kts
+++ ElCinema/build.gradle.kts
@@ -30,9 +30,10 @@ android {
 version = "1.0.0"
 
 cloudstream {
-    language = "ar"
+    name = "ElCinema"
     description = "ElCinema provider"
     authors = listOf("FathedAbOss")
+    language = "ar"
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
 }
