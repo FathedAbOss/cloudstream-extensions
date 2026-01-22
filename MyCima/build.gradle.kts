ce25ca683a86f6f79c3eec40dabcb12d3b72d9ff
FathedAbOss <fathekhateb22@gmail.com>
2026-01-22 20:13:34 +0100

    Update build.gradle.kts

--- MyCima/build.gradle.kts
+++ MyCima/build.gradle.kts
@@ -30,9 +30,10 @@ android {
 version = "1.0.0"
 
 cloudstream {
-    language = "ar"
+    name = "MyCima"
     description = "MyCima provider"
     authors = listOf("FathedAbOss")
+    language = "ar"
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
 }
