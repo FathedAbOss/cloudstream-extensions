3f78b98bf2415f7622ae5c035b829edfaa606679
FathedAbOss <fathekhateb22@gmail.com>
2026-01-23 00:57:23 +0100

    Update BrstejPlugin.kt

--- Brstej/src/main/kotlin/com/example/BrstejPlugin.kt
+++ Brstej/src/main/kotlin/com/example/BrstejPlugin.kt
@@ -1,13 +1,12 @@
 package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class BrstejPlugin: Plugin() {
-      override fun load(context: Context) {
-                // All providers should be registered here
-                registerMainAPI(BrstejProvider())
-      }
+class BrstejPlugin: CloudstreamPlugin() {
+    override fun load(context: Context) {
+        // All providers should be registered here
+        registerMainAPI(BrstejProvider())
+    }
 }
