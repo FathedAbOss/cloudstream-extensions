f079df266fb5cd19e5f25edfb9ba75c91f59f505
FathedAbOss <fathekhateb22@gmail.com>
2026-01-23 00:58:33 +0100

    Update EgyDeadPlugin.kt

--- EgyDead/src/main/kotlin/com/example/EgyDeadPlugin.kt
+++ EgyDead/src/main/kotlin/com/example/EgyDeadPlugin.kt
@@ -1,13 +1,12 @@
 package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class EgyDeadPlugin: Plugin() {
-      override fun load(context: Context) {
-                // All providers should be registered here
-                registerMainAPI(EgyDeadProvider())
-      }
+class EgyDeadPlugin: CloudstreamPlugin() {
+    override fun load(context: Context) {
+        // All providers should be registered here
+        registerMainAPI(EgyDeadProvider())
+    }
 }
