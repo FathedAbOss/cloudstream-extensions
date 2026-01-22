7a59997d9f2ce180a4a3f63cc44287946ec1a1fc
FathedAbOss <fathekhateb22@gmail.com>
2026-01-23 00:59:52 +0100

    Update ElCinemaPlugin.kt

--- ElCinema/src/main/kotlin/com/example/ElCinemaPlugin.kt
+++ ElCinema/src/main/kotlin/com/example/ElCinemaPlugin.kt
@@ -1,13 +1,12 @@
 package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class ElCinemaPlugin: Plugin() {
-      override fun load(context: Context) {
-                // All providers should be registered here
-                registerMainAPI(ElCinemaProvider())
-      }
+class ElCinemaPlugin: CloudstreamPlugin() {
+    override fun load(context: Context) {
+        // All providers should be registered here
+        registerMainAPI(ElCinemaProvider())
+    }
 }
