bcbe7d55417c217995b09e83e817b344a857ff8f
FathedAbOss <fathekhateb22@gmail.com>
2026-01-23 01:13:19 +0100

    Update AkSvPlugin.kt

--- AkSv/src/main/kotlin/com/example/AkSvPlugin.kt
+++ AkSv/src/main/kotlin/com/example/AkSvPlugin.kt
@@ -1,10 +1,11 @@
 package com.example
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class AkSvPlugin: CloudstreamPlugin() {
+class AkSvPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be registered here
         registerMainAPI(AkSvProvider())
