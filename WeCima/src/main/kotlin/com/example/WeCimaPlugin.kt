d7be8c999f66a6075c0c3197644db762e8b49bb3
FathedAbOss <fathekhateb22@gmail.com>
2026-01-23 03:17:16 +0100

    Update WeCimaPlugin.kt

--- WeCima/src/main/kotlin/com/example/WeCimaPlugin.kt
+++ WeCima/src/main/kotlin/com/example/WeCimaPlugin.kt
@@ -1,13 +1,12 @@
 package com.example
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class WeCimaPlugin: CloudstreamPlugin() {
+class WeCimaPlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be registered here
         registerMainAPI(WeCimaProvider())
     }
 }
