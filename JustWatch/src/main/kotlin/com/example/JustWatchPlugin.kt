e271e94cc23804c435b8280803c26469b59f171b
FathedAbOss <fathekhateb22@gmail.com>
2026-01-23 03:13:54 +0100

    Update JustWatchPlugin.kt

--- JustWatch/src/main/kotlin/com/example/JustWatchPlugin.kt
+++ JustWatch/src/main/kotlin/com/example/JustWatchPlugin.kt
@@ -1,13 +1,12 @@
 package com.example
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class JustWatchPlugin: CloudstreamPlugin() {
+class JustWatchPlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be registered here
         registerMainAPI(JustWatchProvider())
     }
 }
