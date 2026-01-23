d83f28d58e6b2c549ee2af897945f375b7a1c5d1
FathedAbOss <fathekhateb22@gmail.com>
2026-01-23 03:16:08 +0100

    Update MyCimaPlugin.kt

--- MyCima/src/main/kotlin/com/example/MyCimaPlugin.kt
+++ MyCima/src/main/kotlin/com/example/MyCimaPlugin.kt
@@ -1,13 +1,12 @@
 package com.example
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class MyCimaPlugin: CloudstreamPlugin() {
+class MyCimaPlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be registered here
         registerMainAPI(MyCimaProvider())
     }
 }
