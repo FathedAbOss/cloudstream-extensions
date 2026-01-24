782ec6aba9d34e5185d507edacac452f5dd3eb8e
FathedAbOss <fathekhateb22@gmail.com>
2026-01-24 17:41:04 +0100

    Update LaroozaPlugin.kt

--- Larooza/src/main/kotlin/com/example/LaroozaPlugin.kt
+++ Larooza/src/main/kotlin/com/example/LaroozaPlugin.kt
@@ -1,10 +1,9 @@
 package com.example
 
 import android.content.Context
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.registerMainAPI
 
-@CloudstreamPlugin
 class LaroozaPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(LaroozaProvider())
