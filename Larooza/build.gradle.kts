dbec5c25d2b8654cb2b6f95317eacb8f6cbbf14e
FathedAbOss <fathekhateb22@gmail.com>
2026-01-31 00:16:34 +0100

    Update build.gradle.kts

--- Larooza/build.gradle.kts
+++ Larooza/build.gradle.kts
@@ -1,7 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 cloudstream {
-    description = "Larooza provider"
+    description = "Cima4U (cfu.cam) provider"
     authors = listOf("FathedAbOss")
     language = "ar"
     status = 1
-    tvTypes = listOf("Movie", "TvSeries")
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
+}
+
+/**
+ * CI FIX: do not fail build on warnings for this module
+ */
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        allWarningsAsErrors = false
+        freeCompilerArgs = freeCompilerArgs.filterNot { it == "-Werror" }
+    }
 }
