c20fc844fd00faab38eb5698194efb4ab412f9d5
FathedAbOss <fathekhateb22@gmail.com>
2026-01-31 09:54:43 +0100

    Update build.gradle.kts

--- Larooza/build.gradle.kts
+++ Larooza/build.gradle.kts
@@ -1,39 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.example"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-tasks.withType<KotlinCompile>().configureEach {
-    compilerOptions {
-        allWarningsAsErrors.set(false)
-
-        val current = freeCompilerArgs.getOrElse(emptyList())
-        freeCompilerArgs.set(current.filterNot { it == "-Werror" })
-
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-    }
-}
-
 cloudstream {
     description = "Cima4U provider"
     authors = listOf("FathedAbOss")
     language = "ar"
     status = 1
     tvTypes = listOf("Movie", "TvSeries", "Anime")
+}
+
+android {
+    namespace = "com.example"
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        allWarningsAsErrors.set(false)
+        freeCompilerArgs.set(freeCompilerArgs.get().filterNot { it == "-Werror" })
+    }
 }
